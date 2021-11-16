a77fd2dd5e469db7c40f8a8e27177c1c57430027
saippuakauppias <945306+saippuakauppias@users.noreply.github.com>
2021-11-16 19:16:30 +0300

    fix: dont use notification signals in tests

--- notificationsservice/notifications/tests/factories.py
+++ notificationsservice/notifications/tests/factories.py
@@ -1,10 +1,15 @@
 from datetime import timedelta
 
+from django.db.models.signals import post_save, pre_delete
 from django.utils import timezone
 from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
 
 from notificationsservice.notifications.models import Notification
+from notificationsservice.notifications.signals import (
+    notification_delete,
+    notification_save,
+)
 from notificationsservice.users.tests.factories import UserFactory
 
 
@@ -17,3 +22,10 @@ class NotificationFactory(DjangoModelFactory):
     class Meta:
         model = Notification
         django_get_or_create = ("title",)
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        post_save.disconnect(notification_save, sender=model_class)
+        pre_delete.disconnect(notification_delete, sender=model_class)
+
+        return super(NotificationFactory, cls)._create(model_class, *args, **kwargs)
